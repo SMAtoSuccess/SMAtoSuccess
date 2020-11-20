b2ca8f437e7bd142f4de7a31b45ac626ab912a87
Alexandra <anance5@murraystate.edu>
2020-11-19 20:07:26 -0600

    started on logging out feature

--- controllers/api-routes/user.js
+++ controllers/api-routes/user.js
@@ -67,6 +67,17 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+          res.status(204).end();
+        });
+      }
+      else {
+        res.status(404).end();
+      }
+});
+
 // POST /api
 router.post('/', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
